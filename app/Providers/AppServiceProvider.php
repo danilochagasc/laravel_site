0f75e8de426fb43917be4260ce0d8d592a324dd4
DaniloChagas <danilovga1000@gmail.com>
2021-09-17 17:44:36 -0300

    heroku?

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->enviroment('production')){
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
-        }
+          }
     }
 }
